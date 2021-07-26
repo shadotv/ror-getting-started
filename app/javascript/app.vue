ccef83ea8ea3bd67d0f518cc81b226f3292c5f5f
Stefano Sello <ssello@shado.tv>
2021-07-26 14:39:02 +0200

    Added Vue

---
+++ app/javascript/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Benvenuto in SHADO!"
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$font-size: 2rem;
+
+p {
+  font-size: $font-size;
+  text-align: center;
+}
+</style>
